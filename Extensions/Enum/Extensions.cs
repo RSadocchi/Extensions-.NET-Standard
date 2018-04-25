6b6b2bd6ed34a64d35de1938e718d516229de322
RSado <r.sadocchi@outlook.it>
2018-04-25 18:02:17 +0200

    Enum Ext.

--- Extensions/Enum/Extensions.cs
+++ Extensions/Enum/Extensions.cs
@@ -7,5 +7,15 @@ namespace Extensions.Enum
 {
     public static class Extensions
     {
+        public static string GetStringValue<T>(this T source) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum) return null;
+
+            Type attr = null;
+            StringValue value = (StringValue)Attribute.GetCustomAttribute(attr, typeof(StringValue));
+            if (value == null) return null;
+
+            return value.Value;
+        }
     }
 }
---
+++ Extensions/Enum/StringValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Extensions.Enum
+{
+    [AttributeUsage(AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
+    public class StringValue : Attribute
+    {
+        public string Value { get; private set; }
+        public StringValue(string value)
+        {
+            Value = value;
+        }
+    }
+}
