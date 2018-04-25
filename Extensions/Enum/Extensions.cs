9f4ace4ec20d23b93f7d0a8ddb76e5ca80b502a9
RSado <r.sadocchi@outlook.it>
2018-04-25 17:35:59 +0200

    Extensins - .Net Standard
    Provide some easy but useful extension for Linq, Enum an other

---
+++ Extensions/Enum/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Extensions.Enum
+{
+    public static class Extensions
+    {
+    }
+}
